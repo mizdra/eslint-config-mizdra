843dd10de4c0276444eb3cc4a81c328cb8a263c5
mizdra <pp.mizdra@gmail.com>
2020-12-02 01:48:54 +0900

    material-ui に関する特別ルールはドメスティックなので消してしまう

--- index.js
+++ index.js
@@ -38,9 +38,5 @@ module.exports = {
     'import/no-default-export': 2,
     // import 文の並びについて思考するのに時間を費やすのは勿体ないので、一律でソートしてしまう
     'import/order': [2, { alphabetize: { order: 'asc' } }],
-    // `@material-ui/{core,icons}` を直接 import するとビルドや tsserver の応答が遅くなるので,
-    // `@material-ui/{core,icons}` の import を禁止する
-    // ref: https://material-ui.com/guides/minimizing-bundle-size/#how-to-reduce-the-bundle-size
-    'no-restricted-imports': ['error', { paths: ['@material-ui/core', '@material-ui/icons'] }],
   },
 };
