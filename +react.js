87e50e3ee28f6b4b7ae1bc938446a29bbf3491a7
mizdra <pp.mizdra@gmail.com>
2023-04-15 15:50:39 +0900

    TypeScript の型表現が充実してきて propTypes 不要になりつあるので、props-types はやめる

--- +react.js
+++ +react.js
@@ -1,14 +1,5 @@
 // @ts-check
 
-/** @type import('eslint').Linter.ConfigOverride */
-const configOverrideForTS = {
-  files: ['*.ts', '*.tsx', '*.cts', '*.mts'],
-  rules: {
-    // TypeScript では propTypes は使わず TypeScript の型注釈を使えば良いので off にする
-    'react/prop-types': 0,
-  },
-};
-
 /** @type import('eslint').Linter.BaseConfig */
 module.exports = {
   plugins: ['react', 'react-hooks'],
@@ -34,6 +25,7 @@ module.exports = {
     // react
     // コーディングスタイル統一のため、`<Component />` の形式で記述できる場合はそのように記述する
     'react/self-closing-comp': 2,
+    // 現代では TypeScript で型を制限することが多いので、propTypes は使わない
+    'react/prop-types': 0,
   },
-  overrides: [configOverrideForTS],
 };
