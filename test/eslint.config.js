5d36f9deccb6c257234140f7c3f5f03cb7dc33e0
mizdra <pp.mizdra@gmail.com>
2024-09-16 16:54:03 +0900

    legacy config 向けのテストを削除

--- test/eslint.config.js
+++ test/eslint.config.js
@@ -4,7 +4,7 @@ import mizdra from '../src/flat/index.js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['.eslintrc.cjs', 'eslint.config.js'] },
+  { ignores: ['eslint.config.js'] },
   ...mizdra.baseConfigs,
   ...mizdra.typescriptConfigs,
   ...mizdra.nodeConfigs,
