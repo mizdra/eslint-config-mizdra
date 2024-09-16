2093bed8a4de5ffdebd2ff81d5b33d1f16b226db
mizdra <pp.mizdra@gmail.com>
2024-09-16 18:48:59 +0900

    eslint-plugin-import に依存しなくなったので eslint-plugin-import 向けの特別対応を削除

--- src/configs/base.js
+++ src/configs/base.js
@@ -15,13 +15,6 @@ export const baseConfigs = [
     },
     languageOptions: {
       sourceType: 'module',
-      parserOptions: {
-        // Flat Config では `parserOptions` に `ecmaVersion`/`sourceType` は存在しないことになっているが、
-        // Legacy Config 時代に作られた custom rule では以前として `parserOptions.ecmaVersion` などを参照している。
-        // そのため、`parserOptions` に `ecmaVersion`/`sourceType` を設定しておく。
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
     },
     rules: {
       // ***** eslint *****
@@ -194,9 +187,6 @@ export const baseConfigs = [
     files: [commonjsPattern],
     languageOptions: {
       sourceType: 'commonjs',
-      parserOptions: {
-        sourceType: 'commonjs',
-      },
     },
   },
 ];
