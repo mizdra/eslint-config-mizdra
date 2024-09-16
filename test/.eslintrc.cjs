bb954fe62b1f1a0d93817fc981351893b424c51e
mizdra <pp.mizdra@gmail.com>
2024-09-16 16:30:25 +0900

    add TODO comments

--- test/.eslintrc.cjs
+++ test/.eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
       files: ['+node.cjs'],
       extends: ['../src/+node.cjs'],
     },
+    // TODO: ESLint v9 + Legacy Config では eslint-plugin-react-hooks が動かないので、一旦コメントアウト
     // {
     //   files: ['+react.tsx'],
     //   extends: ['../src/+react.cjs'],
