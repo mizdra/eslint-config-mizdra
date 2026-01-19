687dd98fa730464717dd82071a2d66b890310716
mizdra <pp.mizdra@gmail.com>
2026-01-19 23:30:38 +0900

    refactor: 不要になった workaround を削除

--- src/util.js
+++ src/util.js
@@ -1,8 +1,3 @@
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __dirname = new URL('.', import.meta.url).pathname;
-export const compat = new FlatCompat({ resolvePluginsRelativeTo: __dirname });
-
 export const jsPattern = '**/*.{js,jsx,mjs,cjs}';
 export const tsPattern = '**/*.{ts,tsx,mts,cts}';
 export const commonjsPattern = '**/*.{cjs,cts}';
